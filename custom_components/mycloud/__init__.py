bf162f264d2d38e38973519fc651d12ceb5e5656
J-shw <91017208+J-shw@users.noreply.github.com>
2025-08-06 21:45:25 +0100

    Update __init__.py

--- custom_components/mycloud/__init__.py
+++ custom_components/mycloud/__init__.py
@@ -1,15 +1,20 @@
 import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-from .const import DOMAIN, CONF_API_KEY
+from .const import DOMAIN, HOST, USERNAME, PASSWORD
+
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    _LOGGER.info("Setting up AirVPN integration")
+    _LOGGER.info("Setting up My Cloud integration")
 
-    api_key = entry.data["api_key"]
-    hass.data.setdefault(DOMAIN, {})[CONF_API_KEY] = api_key
+    host = entry.data["Host"]
+    username = entry.data["Username"]
+    password = entry.data["Password"]
+    hass.data.setdefault(DOMAIN, {})[HOST] = host
+    hass.data.setdefault(DOMAIN, {})[USERNAME] = username
+    hass.data.setdefault(DOMAIN, {})[PASSWORD] = password
 
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
