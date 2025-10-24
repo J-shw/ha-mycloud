39e51cd9c06f039487fbb6bac312bdec4e8d075c
Josh <91017208+J-shw@users.noreply.github.com>
2025-10-24 16:13:15 +0100

    Added version

--- custom_components/mycloud/__init__.py
+++ custom_components/mycloud/__init__.py
@@ -1,7 +1,7 @@
 import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-from .const import DOMAIN, HOST, USERNAME, PASSWORD
+from .const import DOMAIN, HOST, USERNAME, PASSWORD, VERSION
 
 
 _LOGGER = logging.getLogger(__name__)
@@ -12,9 +12,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     host = entry.data["Host"]
     username = entry.data["Username"]
     password = entry.data["Password"]
+    version = entry.data["Version"]
+
     hass.data.setdefault(DOMAIN, {})[HOST] = host
     hass.data.setdefault(DOMAIN, {})[USERNAME] = username
     hass.data.setdefault(DOMAIN, {})[PASSWORD] = password
+    hass.data.setdefault(DOMAIN, {})[VERSION] = version
+
 
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
